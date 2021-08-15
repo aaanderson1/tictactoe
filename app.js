71989e655a5ae94307a11a7a04a6793c62c7f402
Allison Anderson <andersonaallison@gmail.com>
2021-08-14 18:05:46 -0700

    updating square names

--- app.js
+++ app.js
@@ -13,7 +13,14 @@ const handleReset = (e) => {
     console.log(e);
 };
 
+const handleCellClick = (e) => {
+    console.log(e.target.classList);
+};
+
 //event listeners
 resetDiv.addEventListener('click', handleReset);
 
+for (const cellDiv of cellDivs) {
+cellDiv.addEventListener('click', handleCellClick)
+};
 
