eacc2eecfadcbddbe63a4043fa10548015f6b2f4
Allison Anderson <andersonaallison@gmail.com>
2021-08-14 19:14:33 -0700

    updated js

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ cont cellDivs = document.querySelectorAll('.game-cell');
 let gameIsLove = true;
 let xIsNext = true;
 
+//functions
+const checkGameStatus = () => {
+    const topLeft = cellDivs[0].classList[1];
+    const topMiddle = cellDivs[1].classList[1];
+    const topRight = cellDivs[2].classList[1];
+    const middleLeft = cellDivs[3].classList[1];
+    const middleMiddle = cellDivs[4].classList[1];
+    const middleRight = cellDivs[5].classList[1];
+    const bottomLeft = cellDivs[6].classList[1];
+    const bottomMiddle = cellDivs[7].classList[1];
+    const bottomRight = cellDivs[8].classList[1];
+
+}
+
+
+
+
+
+
 //event Handlers
 const handleReset = (e) => {
     console.log(e);
@@ -17,7 +36,9 @@ const handleCellClick = (e) => {
     const classList = e.target.classList;
     const location = classList[1];
 
-if ()
+if (classList[2] == 'x' || classList[2] == 'o') {
+    return; 
+}
 
     if (xIsNext) {
 
