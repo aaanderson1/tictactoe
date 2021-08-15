37a98262253ebc4a466d4a45b76dd276ef6d07e3
Allison Anderson <andersonaallison@gmail.com>
2021-08-15 10:23:12 -0700

    Update app.js
    Changed characters

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const resetDiv = document.querySelector('.reset');
 const cellDivs = document.querySelectorAll('.game-cell');
 
 //game constants
-const xSymbol = 'x';
-const oSymbol = 'o';
+const xSymbol = '×';
+const oSymbol = '○';
 
 //game variables
 let gameIsLive = true;
