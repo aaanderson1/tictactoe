3241546eeff722ff36f301e1cef9b1aeadc5f978
Allison Anderson <andersonaallison@gmail.com>
2021-08-14 20:30:08 -0700

    Update app.js
    logic JS

--- app.js
+++ app.js
@@ -18,29 +18,47 @@ let winner = null;
 
 //functions
 const letterToSymbol = (letter) => letter == 'x' ? xSymbol : oSymbol;
-
-//functions
-const checkGameStatus = () => {
-    const topLeft = cellDivs[0].classList[2];
-    const topMiddle = cellDivs[1].classList[2];
-    const topRight = cellDivs[2].classList[2];
-    const middleLeft = cellDivs[3].classList[2];
-    const middleMiddle = cellDivs[4].classList[2];
-    const middleRight = cellDivs[5].classList[2];
-    const bottomLeft = cellDivs[6].classList[2];
-    const bottomMiddle = cellDivs[7].classList[2];
-    const bottomRight = cellDivs[8].classList[2];
-
-//check winner
-if(topLeft && topLeft === topMiddle && topLeft === topRight) {
+const handleWin = (letter) => {
     gameIsLive = false;
-    winner = topLeft;
-    if (topLeft === 'x') {
-statusDiv.innerHTML = `${letterToSymbol(topLeft)} has won!`;
-} else {
-    statusDiv.innerHTML = `<span>${letterToSymbol({topLeft)} has won!`;
+    winner = letter;
+    if (winner === 'x') {
+        statusDiv.innerHTML = `${letterToSymbol(winner)} has won!`;
+    } else {
+        statusDiv.innerHTML = `<span>
+        ${letterToSymbol({ winner)} has won!
+        </span>`;
     }
-}
+
+
+    //functions
+    const checkGameStatus = () => {
+        const topLeft = cellDivs[0].classList[2];
+        const topMiddle = cellDivs[1].classList[2];
+        const topRight = cellDivs[2].classList[2];
+        const middleLeft = cellDivs[3].classList[2];
+        const middleMiddle = cellDivs[4].classList[2];
+        const middleRight = cellDivs[5].classList[2];
+        const bottomLeft = cellDivs[6].classList[2];
+        const bottomMiddle = cellDivs[7].classList[2];
+        const bottomRight = cellDivs[8].classList[2];
+
+        //check winner
+        if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+            handleWin(topLeft);
+        } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+            handleWin(middleLeft);
+        } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+            handleWin(bottomLeft);
+        } else if (topLeft && topLeft === middleLeft && topMiddle === bottomMiddle) {
+            handleWin(topLeft);
+        } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+            handleWin(topMiddle);
+        } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+            handleWin(topRight);
+        } else if (topRight && topRight === middleRight && middleRight === topRight)
+            handleWin(bottomLeft);
+    }
+    }
 };
 
 //event Handlers
@@ -52,25 +70,25 @@ const handleCellClick = (e) => {
     const classList = e.target.classList;
     const location = classList[1];
 
-if (classList[2] == 'x' || classList[2] == 'o') {
-    return; 
-}
+    if (classList[2] == 'x' || classList[2] == 'o') {
+        return;
+    }
 
     if (xIsNext) {
 
-e.target.classList.add('x');
-xIsNext = !xIsNext;
-} else {
-e.target.classList.add('o')
-xIsNext = !xIsNext; 
+        e.target.classList.add('x');
+        xIsNext = !xIsNext;
+    } else {
+        e.target.classList.add('o')
+        xIsNext = !xIsNext;
 
-}
+    }
 };
 
 //event listeners
 resetDiv.addEventListener('click', handleReset);
 
 for (const cellDiv of cellDivs) {
-cellDiv.addEventListener('click', handleCellClick)
+    cellDiv.addEventListener('click', handleCellClick)
 };
 
